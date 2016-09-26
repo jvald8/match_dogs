0760db8c63ca2301c0b62428d6e62a9539401c15
Jon <jvald8@gmail.com>
2016-09-26 13:45:24 -0700

    fix webpack 6 bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 'use strict';
 
-const HtmlPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 
 module.exports = {
-    entry: "app/index.jsx",
-    output: {
-        path: __dirname,
-        filename: "dist/bundle.js"
-    },
+    entry: [
+        './app/index.js'
+    ],
     module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.jsx?$/,exclude: 'node_modules/', loader: "babel", query:{ presets: ['react','es2015']}}
-        ]
+        loaders: [{test: /\.jsx?$/, include: __dirname + '/app', exclude: /node_modules/, loader: "babel"}]
     },
-    plugins: [new HtmlPlugin({
-        title: 'CustomTitle',
-        template: 'index.html',
-        inject: 'body'
-    })]
-};
+    output : {
+        filename: "index_bundle.js",
+        path: __dirname + '/dist'
+    },
+    plugins: [HTMLWebpackPluginConfig]
+}
